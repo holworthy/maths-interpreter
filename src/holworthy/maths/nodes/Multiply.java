dba22189bed693cfa92f9b10db95479eb151186d
Dan Gee <dangee1705@gmail.com>
2021-10-01 16:53:14 +0100

    brackets

--- src/holworthy/maths/nodes/Multiply.java
+++ src/holworthy/maths/nodes/Multiply.java
@@ -7,7 +7,7 @@ public class Multiply extends BinaryNode {
 
 	@Override
 	public String toString() {
-		return getLeft() + "*" + getRight();
+		return (getLeft() instanceof Add ? "(" + getLeft() + ")" : getLeft()) + "*" + (getRight() instanceof Add ? "(" + getRight() + ")" : getRight());
 	}
 
 	@Override
@@ -15,14 +15,21 @@ public class Multiply extends BinaryNode {
 		Node left = getLeft().simplify();
 		Node right = getRight().simplify();
 
+		// Multiply two numbers together
 		if(left instanceof Number && right instanceof Number)
 			return new Number(((Number) left).getValue() * ((Number) right).getValue());
 	
+		// Make tree left leaning
 		if(right instanceof Multiply)
 			return new Multiply(new Multiply(left, ((Multiply) right).getLeft().simplify()).simplify(), ((Multiply) right).getRight());
 
+		// Expanding brackets
 		if(right instanceof Add)
-			return new Add(new Multiply(left, ((Add) right).getLeft()), new Multiply(left, ((Add) right).getRight()));
+			return new Add(new Multiply(left, ((Add) right).getLeft().simplify()).simplify(), new Multiply(left, ((Add) right).getRight().simplify()).simplify());
+
+		// Putting brackets on the right (which then calls the rule above)
+		if(left instanceof Add)
+			return new Multiply(right, left).simplify();
 
 		return new Multiply(left, right);
 	}
