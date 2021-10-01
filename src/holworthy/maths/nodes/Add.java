06680ae38e4bdb4014fc76be1f2b490297a39d83
Jack Demonakis <jackdemonakis@gmail.com>
2021-10-01 17:47:33 +0100

    changing add to collapse

--- src/holworthy/maths/nodes/Add.java
+++ src/holworthy/maths/nodes/Add.java
@@ -11,38 +11,30 @@ public class Add extends BinaryNode {
 	}
 
 	@Override
-	public Node simplify() {
-		Node left = getLeft().simplify();
-		Node right = getRight().simplify();
+	public Node collapse() {
+		Node left = getLeft().collapse();
+		Node right = getRight().collapse();
 
 		if(left instanceof Number && right instanceof Number)
 			return new Number(((Number) left).getValue() + ((Number) right).getValue());
 
 		if(right instanceof Add)
-			return new Add(new Add(left, ((Add) right).getLeft().simplify()).simplify(), ((Add) right).getRight().simplify());
+			return new Add(new Add(left, ((Add) right).getLeft().collapse()).collapse(), ((Add) right).getRight().collapse());
 
 		if(left instanceof Multiply && right instanceof Multiply && ((Multiply) left).getRight().matches(((Multiply) right).getRight()))
-			return new Multiply(new Add(((Multiply) left).getLeft(), ((Multiply) right).getLeft()).simplify(), ((Multiply) left).getRight());
+			return new Multiply(new Add(((Multiply) left).getLeft(), ((Multiply) right).getLeft()).collapse(), ((Multiply) left).getRight());
 
 		if(left instanceof Variable && right instanceof Variable && left.matches(right))
 			return new Multiply(new Number(2), left);
 
-		// if(left instanceof Multiply && right instanceof Multiply){
-		// 	if(((BinaryNode) left).getLeft() instanceof Number && ((BinaryNode) left).getRight() instanceof Variable && ((BinaryNode) right).getLeft() instanceof Number && ((BinaryNode) right).getRight() instanceof Variable && ((BinaryNode) left).getRight().matches(((BinaryNode) right).getRight())){
-		// 		Add a = new Add(((BinaryNode) left).getLeft(), ((BinaryNode) right).getLeft());
-		// 		return new Multiply(a.simplify(), ((BinaryNode) left).getRight());
-		// 	}
-		// 	if(left.getLeft() instanceof Number)
-		// }
-
 		if(left instanceof Multiply && right instanceof Multiply){
 			if(((BinaryNode) left).getRight().matches(((BinaryNode) right).getRight())){
 				Add a = new Add(((BinaryNode) left).getLeft(), ((BinaryNode) right).getLeft());
-				return new Multiply(a.simplify(), ((BinaryNode) right).getRight());
+				return new Multiply(a.collapse(), ((BinaryNode) right).getRight());
 			}
 			if(((BinaryNode) left).getLeft().matches(((BinaryNode) right).getLeft())){
 				Add a = new Add(((BinaryNode) left).getRight(), ((BinaryNode) right).getRight());
-				return new Multiply(a.simplify(), ((BinaryNode) right).getLeft());
+				return new Multiply(((BinaryNode) right).getLeft(), a.collapse());
 			}
 		}
 
