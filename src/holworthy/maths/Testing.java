3347febed599ed2dfb024249988a0acaf772309e
Jack Demonakis <jackdemonakis@gmail.com>
2021-10-14 13:54:33 +0100

    added new tests

--- src/holworthy/maths/Testing.java
+++ src/holworthy/maths/Testing.java
@@ -33,6 +33,16 @@ public class Testing {
 		tests.put("1/4*5", "5/4");
 		tests.put("49^(3/2)", "343"); 
 		tests.put("(1/3)+(2/5)", "11/15");
+		// tests.put("19.14+8.256", "6849/250"); // decimal not handled
+		// tests.put("946.9+381.6", "2655/2");
+		// tests.put("0.2313+0.7567", "247/250");
+
+		// complex arithmetic
+		// tests.put("6^6*36^3", "2176782336"); // number too big
+		// tests.put("27^6*81^-7", "3^-10"); // parser error on negtaive power
+		// tests.put("36^-1/36^-2", "36"); // same as above
+		tests.put("(22/12)*(30/8)/(34/12)", "165/68");
+		// tests.put("(46/18)/(22/9)/(24/7)","161/528"); // crashes
 
 		// basic algebra
 		tests.put("3*a+3*b", "3*(a+b)");
@@ -48,6 +58,12 @@ public class Testing {
 		tests.put("a*a^7", "a^8");
 		tests.put("b*a*b^3*c*a^2", "a^3*b^4*c");
 		tests.put("(x+3)*(x-2)", "x^2+x-6");
+		tests.put("2*j+3*k+10*k+9*j+8*j", "19*j+13*k");
+		tests.put("-8*t+6*v-t-7*v+8*v", "-9*t+7*v");
+		tests.put("k-9*k-5*n+3*m-k", "-9*k+3*m-5*n");
+		// tests.put("5*(4*h-7)-4*(3*h-6)", "8*h-11"); // stack overflow error
+		tests.put("8*(7*x^2-8*y)-8*(3*x^2-9*y)", "32*x^2+8*y");
+		tests.put("5*(13*j-3*k^2)-9*(7*j-6*k^2)", "2*j+39*k^2");
 
 		// negatives
 		tests.put("-5", "-5");
@@ -61,6 +77,7 @@ public class Testing {
 
 		// division
 		tests.put("-(2/-3)", "2/3");
+		tests.put("-0.7=f/-2", "f=7/5");
 
 		// complex numbers
 		
