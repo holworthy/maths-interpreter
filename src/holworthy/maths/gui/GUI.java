0eeb871a19d746fb3091ef03a8372d5339187de8
Dan Gee <dangee1705@gmail.com>
2021-11-30 11:27:55 +0000

    refactor graph

--- src/holworthy/maths/gui/GUI.java
+++ src/holworthy/maths/gui/GUI.java
@@ -10,13 +10,13 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.UIDefaults;
 import javax.swing.UIManager;
 
 import holworthy.maths.Maths;
 import holworthy.maths.exceptions.MathsInterpreterException;
-import holworthy.maths.nodes.BinaryNode;
 import holworthy.maths.nodes.Equation;
 import holworthy.maths.nodes.Node;
 import holworthy.maths.nodes.Variable;
@@ -43,7 +43,10 @@ public class GUI {
 		JFrame window = new JFrame("Maths Interpreter");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setSize(600, 600);
-		window.setLayout(new BorderLayout());
+		
+		JPanel wrapperPanel = new JPanel(new BorderLayout());
+		JScrollPane scrollPane = new JScrollPane(wrapperPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		window.add(scrollPane);
 
 		JPanel inputPanel = new JPanel();
 		inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.X_AXIS));
@@ -51,11 +54,11 @@ public class GUI {
 		inputPanel.add(inputTextField);
 		JButton interpretButton = new JButton("Interpret");
 		inputPanel.add(interpretButton);
-		window.add(inputPanel, BorderLayout.PAGE_START);
+		wrapperPanel.add(inputPanel, BorderLayout.PAGE_START);
 
 		JPanel outputPanel = new JPanel();
 		outputPanel.setLayout(new BoxLayout(outputPanel, BoxLayout.PAGE_AXIS));
-		window.add(outputPanel, BorderLayout.CENTER);
+		wrapperPanel.add(outputPanel, BorderLayout.CENTER);
 
 		// event listeners
 		interpretButton.addActionListener(ae -> {
@@ -92,23 +95,24 @@ public class GUI {
 							solutionsPanel.add(new JLabel(solution.toString()));
 						outputPanel.add(solutionsPanel);
 
-						if(((Equation) simplified).numVariables() == 2) {
-							if(((BinaryNode) simplified).getRight().matches(new Variable("y"))){
-								simplified = new Equation(((BinaryNode) simplified).getRight(), ((BinaryNode) simplified).getLeft());
+						for(Equation solution : solutions) {
+							if(solution.numVariables() == 2) {
+							
+								// graphs
+								JPanel graphPanel = new JPanel();
+								graphPanel.setLayout(new BoxLayout(graphPanel, BoxLayout.PAGE_AXIS));
+								graphPanel.add(new JLabel("Graph of " + solution + ":"));
+								Graph graph = new Graph((Equation) solution);
+	
+								// TODO: specify which variable is on which axis
+								// graph.setXAxis();
+								// graph.setYAxis();
+	
+								graphPanel.add(graph);
+								outputPanel.add(graphPanel);
 							}
-							// graphs
-							JPanel graphPanel = new JPanel();
-							graphPanel.setLayout(new BoxLayout(graphPanel, BoxLayout.PAGE_AXIS));
-							graphPanel.add(new JLabel("Graph:"));
-							Graph graph = new Graph((Equation) simplified);
-
-							// TODO: specify which variable is on which axis
-							// graph.setXAxis();
-							// graph.setYAxis();
-
-							graphPanel.add(graph);
-							outputPanel.add(graphPanel);
 						}
+						
 					}
 
 					// derivative
